338012dd89bd8c1637a34a48d86f215ab6819338
Dmytro Basamyhin <dima17lit@gmail.com>
2025-09-07 19:28:04 +0300

    fix

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,38 +1,34 @@
-'use client';
-
-import Link from 'next/link';
-import css from '@/app/not-found.module.css'
-import type { Metadata } from "next";
+import { Metadata } from "next";
+import css from "./not-found.module.css";
 
 export const metadata: Metadata = {
-  title: "404 - Page Not Found",
-  description: "The page you are looking for does not exist.",
-  alternates: {
-    canonical: "https://yourdomain.com/not-found", // 🔑 абсолютний URL
-  },
+  title: "404 | Page Not Found",
+  description: "This page doesn'n exist",
   openGraph: {
-    title: "404 - Page Not Found",
-    description: "The page you are looking for does not exist.",
-    url: "https://yourdomain.com/not-found", // 🔑 обовʼязково абсолютний URL
+    title: "404 | Page Not Found",
+    description: "This page doesn'n exist",
+    url: "https://08-zustand-ten-ochre.vercel.app/not-found",
     images: [
       {
-        url: "https://via.placeholder.com/1200x630.png?text=404", // 🔑 справжня картинка
+        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
         width: 1200,
         height: 630,
-        alt: "404 - Not Found",
+        alt: "Note Hub Logo",
       },
     ],
   },
 };
 
-const NotFound = () => {
+export default function NotFound() {
   return (
+    <>
       <div className={css.container}>
-          <h1 className={css.title}>404 - Page not found</h1>
-          <p className={css.description}>Sorry, the page you are looking for does not exist.</p>
-          <Link href="/">Go back home</Link>
-    </div>
+        <h1 className={css.error}>404</h1>
+        <p className={css.message}>Oooops! Page not found</p>
+        <p className={css.description}>
+          Sorry, the page you are looking for does not exist.
+        </p>
+      </div>
+    </>
   );
-};
-
-export default NotFound;
+}
